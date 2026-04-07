10bdac934ad0922c55aacbf6b127927c1dbdb48f
Mercuriallz <ramandadiio@gmail.com>
2026-04-07 19:58:37 +0700

    initial commit

---
+++ lib/menu_item.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final Widget page;
+
+  MenuItem({required this.title, required this.page});
+}
---
+++ lib/pertemuan3_.dart
@@ -0,0 +1,54 @@
+import 'package:belajar_flutter/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
+class Pertemuan3 extends StatefulWidget {
+  const Pertemuan3({super.key});
+
+  @override
+  State<Pertemuan3> createState() => _Pertemuan3State();
+}
+
+class _Pertemuan3State extends State<Pertemuan3> {
+  var currentIndex = 0;
+
+  final List<Widget> page = [
+    HomePage(),
+    Container(color: Colors.blueGrey),
+    Container(color: Colors.greenAccent),
+    Container(color: Colors.greenAccent),
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: page[currentIndex],
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: currentIndex,
+        onTap: (i) => setState(() => currentIndex = i),
+        items: [
+          SalomonBottomBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Beranda"),
+            selectedColor: Colors.red,
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.calendar_month),
+            title: Text("Kalender"),
+            selectedColor: Colors.blueAccent,
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.approval_rounded),
+            title: Text("Maps"),
+            selectedColor: Colors.blueGrey,
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.notifications),
+            title: Text("Notifikasi"),
+            selectedColor: Colors.greenAccent,
+          ),
+        ],
+      ),
+    );
+  }
+}
